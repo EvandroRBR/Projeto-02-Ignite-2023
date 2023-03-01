52f242d46e945de6b835a170a44abc4591fc0a9a
EvandroRBR <evandro.braga@beedoo.com.br>
2023-03-01 17:33:14 -0300

    fix: Correção da rota summary para retornar um objeto

--- src/routes/transaction.ts
+++ src/routes/transaction.ts
@@ -55,7 +55,7 @@ export async function transactionsRoutes(app: FastifyInstance) {
         .sum('amount', { as: 'amount' })
         .first();
 
-      return summary;
+      return { summary };
     },
   );
 
